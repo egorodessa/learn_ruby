abc8fcb85e245d9eec8fedec2f3a4862b9091abb
IEgor Dorovskykh <egor.odessa@gmail.com>
2016-02-09 11:44:21 +0200

    04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,35 +2,29 @@ def translate(sentence)
   
   vowels = %w{ a e i o u } 
 
-  sentence.downcase.split(" ").each { |word| 
-    puts word
+  sentence.downcase.split.map { |word| 
+    word.chomp!
 
     if word.length > 2 then
-      case word 
-
-        # 1 vowels
-        when vowels.include?(word[0,1])
-          puts word + "ay"
+      # 1 vowels
+      if vowels.include?(word[0,1]) then word + "ay"
 
         # 1 consonant + exception qu
-        when ( not vowels.include?(word[0,1]) ) && vowels.include?(word[1,1])
-          if word[0,1] == "qu" then puts word[1,word.length-1] + word[0,1] + "ay"
-          else puts word[1,word.length-1] + word[0,1] + "ay" end
+        elsif word[0,2] == "qu" then word[2,word.length-1] + word[0,2] + "ay"
+        elsif ( not vowels.include?(word[0,1]) ) && vowels.include?(word[1,1])
+        word[1,word.length-1] + word[0,1] + "ay"
 
-        # 2 consonants
-        when ( not vowels.include?(word[0,1]) ) && ( not vowels.include?(word[1,1]) )
-          puts word[2,word.length-1] + word[0,2] + "ay"
+          # 2 consonants
+          elsif ( not vowels.include?(word[0,1]) ) && ( not vowels.include?(word[1,1]) ) && vowels.include?(word[2,1]) then
+          word[2,word.length-1] + word[0,2] + "ay"
 
-        # 3 consonants + exception
-        when ( not vowels.include?(word[0,3]) )
-          puts word[3,word.length-1] + word[0,3] + "ay"
-
-      #end
-      else puts "else case" end
-    else 
-      puts "elsif "
-      word end
+            # 3 consonants + exception
+            elsif ( not vowels.include?(word[0,3]) )
+            word[3,word.length-1] + word[0,3] + "ay" end
+              else 
+                word 
+    end
   }.join(" ")
 end
 
-puts translate("my Apple and eggs")
+puts translate("apple nix eat pie stupid three quiet school the quick brown fox")
